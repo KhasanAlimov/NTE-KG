c0aeeb5fcc69916cd0e50491f90e7def18d8dbcd
Khasan Alimov <hasanalimov021@gmail.com>
2021-07-28 11:54:48 +0600

    Housing page rout

--- routes.js
+++ routes.js
@@ -4,16 +4,21 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     res.render('pages/home', {
       title: 'Home',
-      isHome: true,
-      cssLink: 'assets/css/home.css'
+      isHome: true
     });
   });
 
   app.get('/about-us', (req, res) => {
     res.render('pages/aboutUs', {
       title: 'About us',
-      isaboutUs: true,
-      cssLink: 'assets/css/aboutUs.css'
+      isaboutUs: true
+    });
+  });
+
+  app.get('/housing', (req, res) => {
+    res.render('pages/housing', {
+      title: 'Housing',
+      isHousing: true
     });
   });
 
--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,1 @@
 // import _ from 'lodash';
-import '../scss/home.scss';
-console.log('work');
